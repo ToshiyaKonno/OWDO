5b541363008c00e5f1056d77b9daf81a54ae3c5d
ToshiyaKonno <tk.ichinoseki@gmail.com>
2020-11-08 17:28:55 +0900

    完成かも

--- server/resources/views/yakusoku/show.blade.php
+++ server/resources/views/yakusoku/show.blade.php
@@ -3,7 +3,12 @@
 
 @section('content')
 
-<b>お約束事項：{{$user->yakusoku}}</b>
+<h1>お約束事項：</h1>
+
+<div>
+<p>{{$user->yakusoku}}</p>    
+</div>
+
 <a href="{{Auth::id()}}/edit">編集</a>
 
     <a class="dropdown-item" href="{{ route('logout') }}"
@@ -11,6 +16,7 @@
                             document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
     </a>
+    
     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
     </form>
